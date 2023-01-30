ae50f914ab82845983aa12497abff92525a72e33
Kevsely <elegis.sossou@yahoo.com>
2023-01-30 22:00:49 +0100

    Starting implementing saga in the project

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -4,21 +4,14 @@ import { Routes, Route } from 'react-router-dom'
 
 import CategoriesPreview from '../categories-preview/categories-preview.component'
 import Category from '../category/category.component'
+import { fetchCategoriesStart } from '../../store/categories/category.action'
 
 import './shop.styles.scss'
-
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils'
-
-import { setCategories } from '../../store/categories/category.action'
 
 const Shop = () => {
 	const dispatch = useDispatch()
 	useEffect(() => {
-		const getCategoriesMap = async () => {
-			const categoryArray = await getCategoriesAndDocuments()
-			dispatch(setCategories(categoryArray))
-		}
-		getCategoriesMap()
+		dispatch(fetchCategoriesStart())
 	}, [])
 
 	return (
